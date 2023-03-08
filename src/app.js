b5de3d5f1af5866499197b93238acfef2669f153
didiermun <didiermunezer38@gmail.com>
2023-03-08 14:40:30 +0200

    parent 283906343e9306dfcf408634060bd7aedb7ab392 author edine-noella <edinenoella@gmail.com> 1679676147 +0200 committer edine-noella <edinenoella@gmail.com> 1679679866 +0200
    squashed&rebased-again
    
    feat: project structure initialization
    
    feat: add express server starter
    
    feat: add express server starter
    
    feat: add express server starter
    
    added eslint configuration
    
    chore(Eslint):new coding format [Updating # 184638361]
    
    chore(Eslint):updated format rules [Continuation # 184638361]
    
    chore(docker): create Dockerfile for node
    
    - create an image to run express server
    
    [Delivers #184638368]
    
    chore(docker): create docker compose file
    
    - run express server and database in the same container
    
    [Delivers #184638368]
    
    chore(gh-actions): add CI yml file
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): add readme status badge
    
    chore(gh-actions): change branch names for jobs
    
    chore: change workflow name
    
    chore: dropped cd.yml
    
    chore: rename to test ci
    
    chore: add build-ci
    
    chore: postgres and sequelize orm setup
    
    chore: seeding scripts
    
    User Registration and Email Verification
    
    chore(docker): expose app to the correct port
    
    - Run app in container
    
    [Delivers #184638368]
    
    ft: comment out test and build scripts
    
    ft: add pull request template
    
    ft: add pull request template
    
    feat: fixed run commands
    
    feat: modified build script
    
    feat(user): implement user registration
    
    - register user
    
    [Delivers #184638320]
    
    chore: env file setup and database variables
    
    parent 93c2e9699990af0ae7059a33874b2361f6f354da
    author didiermun <didiermunezer38@gmail.com> 1678702941 +0200
    committer nkbtemmy <nkubito@augmented-future.com> 1678808768 +0200
    
    ft: add server starter scripts && es6 configuration - 184639058
    
    Refactoring
    
    User Register Works and refactoring
    
    feat(user): Email Verification
    
    - verify registered user
    
    [Delivers ##184638371]
    
    feat(user): Email Verification
    
    - verify registered user
    
    [Delivers ##184638371]
    
    chore:.env setup and attached Hound badge
    
    feat: added swagger documentation with openain
    
    chore:.env setup and attached Hound badge
    
    feat: documentation from using openAPI to using JS
    
    Separated Functions
    
    ft(Reset-Password): request changing pass via email [Inprogress # 184638375]
    
    ft(resetPassword):swagger Implimentation[Updating # 184638375]
    
    ft(resetPassword):task concluded[Updating # 184638375]
    
    ft(resetPassword):task concluded[# 184638375]

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express, {
+  json
+} from 'express';
+
+// swagger
+import swaggerUI from 'swagger-ui-express';
+// api docs
+import apiDoc from './swagger';
+import connectDB, { sequelize } from './database/config/db';
+
+const app = express();
+require('dotenv').config();
+
+app.use(json())
+const userRoute = require('./routes/userRoute').default;
+app.use('/api/users', userRoute);
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  res.json({
+    status: true,
+    message: 'Our node.js app works'
+  })
+});
+
+// use swagger apis
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDoc));
+app.listen(PORT, async () => {
+  console.log(`App listening on port ${PORT}`)
+  await connectDB();
+  sequelize.sync({ force: false }).then(() => {
+    console.log('✅Synced database successfully...');
+  });
+})
