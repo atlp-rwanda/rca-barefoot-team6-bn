ea18fc85042f4092356d34f35f50b346ad88a8f2
phelixdusengimana <phelixdusengimana@gmail.com>
2023-03-10 15:36:09 +0200

    fix: eslint errors fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 
-const { Swaggiffy } = require('swaggiffy'); // Using require
+const { Swaggiffy } = require('swaggiffy') // Using require
 
 const app = express()
 const port = 3000
 
 // Build Swaggiffy with our express app.
 new Swaggiffy().setupExpress(app).swaggiffy()
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
