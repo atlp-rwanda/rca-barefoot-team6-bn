de90f42a3773a35d8500c801ae3c7b9ee5c16336
edine-noella <edinenoella@gmail.com>
2023-03-12 19:47:50 +0200

    chore(Eslint):updated format rules [Continuation # 184638361]

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const { Swaggiffy } = require('swaggiffy') // Using require
 
 const app = express()
 const port = 3000;
+<<<<<<< HEAD
 
 // Build Swaggiffy with our express app.
 new Swaggiffy().setupExpress(app).swaggiffy()
+=======
+>>>>>>> 7ff185b (chore(Eslint):updated format rules [Continuation # 184638361])
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
