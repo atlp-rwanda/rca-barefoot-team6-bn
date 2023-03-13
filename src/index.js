02308bad0a370c10e8f7b44ab39812f4170b6ff5
phelixdusengimana <phelixdusengimana@gmail.com>
2023-03-13 17:30:20 +0200

    feat: swagger documentation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 
-const { Swaggiffy } = require('swaggiffy'); // Using require
+const { Swaggiffy } = require('swaggiffy') // Using require
 
 const app = express()
-const port = 3000
+const port = 3000;
 
 // Build Swaggiffy with our express app.
 new Swaggiffy().setupExpress(app).swaggiffy()
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
