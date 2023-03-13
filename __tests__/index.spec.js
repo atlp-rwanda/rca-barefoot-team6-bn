f0110d57e39e0823a47df035a5304e163a41094c
divinirakiza <irakizadivin@gmail.com>
2023-03-13 14:20:46 +0200

    ft: setup jest code coverage

---
+++ __tests__/index.spec.js
@@ -0,0 +1,9 @@
+describe("Test function", () => {
+    test("it should add numbers", () => {
+      const num1 = 1, num2 = 2
+      const sum = num1 + num2
+
+      expect(3).toEqual(sum);
+    })
+
+  });
