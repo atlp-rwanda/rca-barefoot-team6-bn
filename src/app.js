23a9d92abd2f93b6e47969c1648bcc2affd2426a
phelixdusengimana <phelixdusengimana@gmail.com>
2023-03-17 17:26:09 +0200

    feat: added swagger documentation with openain

--- src/app.js
+++ src/app.js
@@ -5,13 +5,69 @@ import connectDB, { sequelize } from './database/config/db';
 import dotenv from 'dotenv';
 import userRoute from './routes/userRoute';
 
+// swagger
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
 const app = express();
 dotenv.config();
 
 app.use(json())
 app.use('/api/users', userRoute);
 
+// swagger options
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Barefoot-nomad unflappables APIs',
+      version: '1.0.0',
+      description: 'This is the documentation for Barefoot-nomad unflappables APIs',
+      contact: {
+        name: 'API Support',
+        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
+        email: 'phelixdusengimana@gmail.com'
+      },
+      servers: [{
+        url: 'http://localhost:3000',
+        description: 'Local Host Documentations'
+      },
+      {
+        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
+        description: 'Onrender Documentations'
+      }]
+    }
+  },
+  apis: ['src/app.js', 'src/routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 const PORT = process.env.PORT || 3000;
+
+const db = require('./database/models/index');
+
+db.sequelize.sync()
+  .then(() => {
+    console.log('Synced with database');
+  })
+  .catch((err) => {
+    console.log('Error syncing with database', err);
+  });
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: gets hello world
+ *     tags: [Hello]
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Server error
+ */
 app.get('/', async (req, res) => {
   res.json({
     status: true,
