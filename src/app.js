c9923adf68d4878a461dc42dc968560430c5d66a
phelixdusengimana <phelixdusengimana@gmail.com>
2023-03-17 18:13:19 +0200

    feat: documentation from using openAPI to using JS

--- src/app.js
+++ src/app.js
@@ -7,42 +7,15 @@ import userRoute from './routes/userRoute';
 
 // swagger
 import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
+
+// api docs
+import apiDoc from './swagger';
 
 const app = express();
 dotenv.config();
 
 app.use(json())
 app.use('/api/users', userRoute);
-
-// swagger options
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Barefoot-nomad unflappables APIs',
-      version: '1.0.0',
-      description: 'This is the documentation for Barefoot-nomad unflappables APIs',
-      contact: {
-        name: 'API Support',
-        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
-        email: 'phelixdusengimana@gmail.com'
-      },
-      servers: [{
-        url: 'http://localhost:3000',
-        description: 'Local Host Documentations'
-      },
-      {
-        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
-        description: 'Onrender Documentations'
-      }]
-    }
-  },
-  apis: ['src/app.js', 'src/routes/*.js']
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 
@@ -56,29 +29,14 @@ db.sequelize.sync()
     console.log('Error syncing with database', err);
   });
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: gets hello world
- *     tags: [Hello]
- *     responses:
- *       200:
- *         description: Success
- *       500:
- *         description: Server error
- */
-app.get('/', async (req, res) => {
+app.get('/api/', async (req, res) => {
   res.json({
     status: true,
     message: 'Our node.js app works'
   })
 });
 
-app.listen(PORT, async () => {
-  console.log(`Example app listening on port ${PORT}`)
-  await connectDB();
-  sequelize.sync({ force: false }).then(() => {
-    console.log('✅Synced database successfully...');
-  });
-})
+// use swagger apis
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDoc));
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`))
