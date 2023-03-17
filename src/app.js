07583c98b7f245e804cbfabe08f855700a7e282b
phelixdusengimana <phelixdusengimana@gmail.com>
2023-03-17 18:13:19 +0200

    feat: documentation from using openAPI to using JS

--- src/app.js
+++ src/app.js
@@ -4,41 +4,14 @@ import express, {
 
 // swagger
 import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
+
+// api docs
+import apiDoc from './swagger';
 
 const app = express();
 require('dotenv').config();
 
 app.use(json())
-
-// swagger options
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Barefoot-nomad unflappables APIs',
-      version: '1.0.0',
-      description: 'This is the documentation for Barefoot-nomad unflappables APIs',
-      contact: {
-        name: 'API Support',
-        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
-        email: 'phelixdusengimana@gmail.com'
-      },
-      servers: [{
-        url: 'http://localhost:3000',
-        description: 'Local Host Documentations'
-      },
-      {
-        url: 'https://rca-barefoot-team-6-bn.onrender.com/',
-        description: 'Onrender Documentations'
-      }]
-    }
-  },
-  apis: ['src/app.js', 'src/routes/*.js']
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 3000;
 
@@ -52,23 +25,14 @@ db.sequelize.sync()
     console.log('Error syncing with database', err);
   });
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: gets hello world
- *     tags: [Hello]
- *     responses:
- *       200:
- *         description: Success
- *       500:
- *         description: Server error
- */
-app.get('/', async (req, res) => {
+app.get('/api/', async (req, res) => {
   res.json({
     status: true,
     message: 'Our node.js app works'
   })
 });
+
+// use swagger apis
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDoc));
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`))
