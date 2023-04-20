12a394e2f7e9332cd039135caf26dd9c4132e5b4
dianKoliva <dianpretty01@gmail.com>
2023-04-20 13:26:31 +0200

    new changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,8 @@
 import express from 'express';
 import user from './userRoute';
 import hotel from './hotelRoute';
-<<<<<<< HEAD
-import destination from './destinationRoute';
 import request from './requestRoute'
 import feeback from './feedbackRoute'
-=======
-import request from './requestRoute';
->>>>>>> 0b5820058f81664d642073aa0f252945fa18bf0e
 
 const router = express.Router();
 router.get('/', (req, res) => {
